d0a327df1c1d65d2829da8adee40427b14a6f7cc
Andre Heider <a.heider@gmail.com>
2020-12-16 09:00:59 +0100

    peripheral.joystick: disable lto
    This fails to build with hardening enabled.

--- packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
+++ packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
@@ -14,7 +14,6 @@ PKG_DEPENDS_TARGET="toolchain kodi-platform p8-platform"
 PKG_SECTION=""
 PKG_SHORTDESC="peripheral.joystick: Joystick support in Kodi"
 PKG_LONGDESC="peripheral.joystick provides joystick support and button mapping"
-PKG_BUILD_FLAGS="+lto"
 
 PKG_IS_ADDON="embedded"
 PKG_ADDON_TYPE="kodi.peripheral"
